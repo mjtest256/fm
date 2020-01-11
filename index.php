4e9dea72cdaff322826e08e15198188d92bc5a1a
mjtest256 <majordomo-test@mail.ru>
2020-01-11 06:15:06 +0300

    Реализован показ файлов по 10 начиная с параметра sp

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
+$start_pos = intval($_GET['sp']) ?: 0;
+$step = 10;
 $fl= scandir('..');
 $fc= sizeof($fl);
-for ($i=0;$i<$fc;$i++) echo($fl[$i]."<br/>");
+
+for ($i=start_pos;$i<$start_pos+$step;$i++) echo($fl[$i]."<br/>");
 
 ?>
