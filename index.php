53862cbacaac8a7de070237c9b78511ab06df160
mjtest256 <majordomo-test@mail.ru>
2020-01-11 06:21:37 +0300

    Исправлена ошибка в определении стартовой позиции

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-$start_pos = intval($_GET['sp']) ?: 0;
+//$start_pos = intval($_GET['sp']) ?: 0;
+if (isset($_GET['sp']) $start_pos = intval($_GET['sp']); else $start_pos=0;
 $step = 10;
 $fl= scandir('..');
 $fc= sizeof($fl);
